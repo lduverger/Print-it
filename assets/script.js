551fd984015d7c6c96f5cf69b761e2b75e422006
Loic Duverger <duverger.loic@live.fr>
2023-11-27 16:02:21 +0100

    step 5 : infinite carousel

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,24 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
 //variable
-let arrowRight= document.querySelector(".arrow_right");
+let arrowRight = document.querySelector(".arrow_right");
 let arrowLeft = document.querySelector(".arrow_left");
 let divDots = document.querySelector(".dots");
 let bannerImg = document.querySelector(".banner-img");
@@ -30,11 +30,14 @@ let arrayDot;
 
 
 //loop for create BulletPoint
-for(i=0; i<numberBulletPoint; i++){
+
+for (i = 0; i < numberBulletPoint; i++) {
 	let div = document.createElement("div");
 	div.classList.add("dot");
 	divDots.appendChild(div);
 }
+
+
 
 arrayDot = document.querySelectorAll(".dots .dot");
 
@@ -58,37 +61,30 @@ arrayDot[activeBP].classList.add("dot_selected");
 arrowRight.addEventListener("click", (event) => {
 	console.log("Click on right arrow");
 	changeBanner(event);
-	/*arrayDot[activeBP].classList.remove("dot_selected");
-	activeBP++;
-	arrayDot[activeBP].classList.add("dot_selected");
-	bannerImg.src = "./assets/images/slideshow/" + slides[activeBP].image;
-	bannerText.innerHTML = slides[activeBP].tagLine;*/
-
-
-
-
 });
 
 
 arrowLeft.addEventListener("click", (event) => {
 	console.log("Click on left arrow");
 	changeBanner(event);
-	/*arrayDot[activeBP].classList.remove("dot_selected");
-	activeBP--;
-	arrayDot[activeBP].classList.add("dot_selected");*/
-
 });
 
-function changeBanner(event){
+function changeBanner(event) {
 	console.log(event);
 	console.log(event.target === arrowRight);
 
 	arrayDot[activeBP].classList.remove("dot_selected");
-	if(event.target === arrowRight){
+	if (event.target === arrowRight) {
 		activeBP++;
+		if (activeBP > 3) {
+			activeBP = 0;
+		}
 	}
-	else{
+	else {
 		activeBP--;
+		if (activeBP < 0) {
+			activeBP = numberBulletPoint - 1;
+		}
 	}
 	arrayDot[activeBP].classList.add("dot_selected");
 	bannerImg.src = "./assets/images/slideshow/" + slides[activeBP].image;
